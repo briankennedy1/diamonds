56e1375a2eb3afb6c05c56b8c403ca274660b108
Brian Kennedy <briankennedy1@gmail.com>
2015-11-25 15:47:53 -0800

    Get semantic ui added in and running

--- entry.js
+++ entry.js
@@ -1,8 +1,10 @@
 var $ = require('jquery');
 window.jQuery = $;
 require("./semantic/dist/semantic.css");
+require("./semantic/dist/semantic.js");
 
 $(document).ready(function() {
+  $('.button').popup();
   $('h1').css('color','red');
   $('button').on('click',function(){
     if ($('h1').css('color') == 'rgb(0, 0, 255)') {
