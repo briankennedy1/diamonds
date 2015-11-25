ac7f0a9937c3f426db575e61368521f8c1534ed1
Brian Kennedy <briankennedy1@gmail.com>
2015-11-25 11:08:34 -0800

    Play with colors to verify it's working

--- entry.js
+++ entry.js
@@ -3,5 +3,11 @@ var $ = require('jquery');
 
 $(document).ready(function() {
   $('h1').css('color','red');
-  $('button').on('click',function(){console.log('clicked');});
+  $('button').on('click',function(){
+    if ($('h1').css('color') == 'rgb(0, 0, 255)') {
+      $('h1').css('color','green');
+    } else {
+      $('h1').css('color','blue');
+    }
+  });
 });
